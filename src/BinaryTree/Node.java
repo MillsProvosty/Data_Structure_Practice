ec38aecb5e2cead59ec66f53d91710281a183270
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2020-04-02 12:12:48 -0600

    NODE- create node class

---
+++ src/BinaryTree/Node.java
@@ -0,0 +1,11 @@
+package BinaryTree;
+
+public class Node {
+    int value;
+        Node left, right;
+        Node(int value){
+            this.value = value;
+            left = null;
+            right = null;
+        }
+}
