3964f6488a4f328d3c6e117278ec488450d3d2fd
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2020-04-02 12:52:55 -0600

    NODE- insert method

--- src/BinaryTree/Node.java
+++ src/BinaryTree/Node.java
@@ -8,4 +8,20 @@ public class Node {
             left = null;
             right = null;
         }
+
+        public void insert(Node node, int value){
+            if (value < node.value) {
+                if (node.left != null) {
+                    insert(node.left, value);
+                } else {
+                    node.left = new Node(value);
+                }
+            } else if (value > node.value){
+                    if (node.right != null) {
+                        insert(node.right, value);
+                    } else {
+                        node.right = new Node(value);
+                }
+            }
+        }
 }
