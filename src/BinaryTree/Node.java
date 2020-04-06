de65098d9eea494791beae1615d907bc05d85166
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2020-04-06 12:40:02 -0600

    Cleaning up some Node code

--- src/BinaryTree/Node.java
+++ src/BinaryTree/Node.java
@@ -10,15 +10,15 @@ public class Node {
         }
 
         public void insert(Node node, int value) {
-            if (value < node.value) {
+            if (value > node.value) {
                 if (node.left != null) {
-                    System.out.println("Left's not null, inserting here");
+                    System.out.println("Inserted " + value + " to left of " + node.value);
                     insert(node.left, value);
                 } else {
-                    System.out.println("Left's was null, creating new node");
+                    System.out.println("Inserted " + value + " to right of " + node.value);
                     node.left = new Node(value);
                 }
-            } else if (value > node.value) {
+            } else if (value < node.value) {
                 if (node.right != null) {
                     System.out.println("Right's not null, inserting here");
                     insert(node.right, value);
@@ -27,4 +27,13 @@ public class Node {
                 node.right = new Node(value);
             }
         }
+
+        public void traverseInOrder(Node node) {
+            if (node != null) {
+                traverseInOrder(node.left);
+                System.out.print(" " + node.value);
+                traverseInOrder(node.right);
+                System.out.print(" " + node.value);
+            }
+    }
 }
--- src/BinaryTree/NodeTest.java
+++ src/BinaryTree/NodeTest.java
@@ -16,7 +16,7 @@ class NodeTest {
     }
 
     @Test
-    void canYouAddNodes() {
+    void canNodesBeAdded() {
         //given
         final Node node2 = new Node(50);
         //when
@@ -25,12 +25,30 @@ class NodeTest {
 
         //then
         assertEquals(node2.value, 50);
-        assertEquals(node2.left.value, 40);
-        assertEquals(node2.right.value, 60);
-        assertNotEquals(node2.left.value, 60);
-        assertNotEquals(node2.right.value, 40);
+        assertEquals(node2.right.value, 40);
+        assertEquals(node2.left.value, 60);
+        assertNotEquals(node2.right.value, 60);
+        assertNotEquals(node2.left.value, 40);
+    }
 
+    @Test
+    void canNodesBeTraversed() {
 
+        //given
+        final Node node3 = new Node(50);
+        //when
+        node3.insert(node3,10);
+        node3.insert(node3,20);
+        node3.insert(node3,30);
+        node3.insert(node3,40);
+        node3.insert(node3,60);
+        node3.insert(node3,70);
+        node3.insert(node3,80);
+        node3.insert(node3,90);
+        node3.insert(node3,100);
+
+        //then
+        node3.traverseInOrder(node3);
     }
 
 
