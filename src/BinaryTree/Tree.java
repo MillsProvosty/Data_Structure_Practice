71bc0234465e8b635468c73a2a70f3203702814a
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2020-04-08 11:41:55 -0600

    Tree - working towards delete

--- src/BinaryTree/Tree.java
+++ src/BinaryTree/Tree.java
@@ -1,7 +1,16 @@
 package BinaryTree;
 
+import static jdk.nashorn.internal.objects.NativeMap.delete;
+
 public class Tree {
+    private Node root;
+
+    public boolean isEmpty() {
+        return this.root == null;
+    }
+
     public Tree(Node node) {
+
     }
 
     static class Node {
@@ -15,24 +24,32 @@ public class Tree {
         }
     }
 
-    public void insert(Node node, int value) {
-        if (value < node.value) {
-            if (node.left != null) {
-                System.out.println("Inserted " + value + " to the left of " + node.value);
-                insert(node.left, value);
-            } else {
-                System.out.println("Created new node " + value + " to left of " + node.value);
-                node.left = new Node(value);
-            }
-        } else if (value > node.value) {
-            if (node.right != null) {
-                System.out.println("Inserted " + value + " to the right of " + node.value);
+    public Node getRoot() {
+        return this.root;
+    }
 
-                insert(node.right, value);
-            } else {
-                System.out.println("Created new node " + value + " to right of " + node.value);
-                node.right = new Node(value);
+    public void insert(Node node, int value) {
+        if (!this.isEmpty()) {
+            if (value < node.value) {
+                if (node.left != null) {
+                    System.out.println("Inserted " + value + " to the left of " + node.value);
+                    insert(node.left, value);
+                } else {
+                    System.out.println("Created new node " + value + " to left of " + node.value);
+                    node.left = new Node(value);
+                }
+            } else if (value > node.value) {
+                if (node.right != null) {
+                    System.out.println("Inserted " + value + " to the right of " + node.value);
+
+                    insert(node.right, value);
+                } else {
+                    System.out.println("Created new node " + value + " to right of " + node.value);
+                    node.right = new Node(value);
+                }
             }
+        } else {
+            root = new Node(value);
         }
     }
     public void traverseInOrder(Node node) {
@@ -41,5 +58,14 @@ public class Tree {
             System.out.print(" " + node.value);
             traverseInOrder(node.right);
         }
+    }
+
+
+    public void deleteNode(Node node, int value) {
+        do {
+            traverseInOrder(node);
+        } while(value == node.value); {
+                delete(node, value);
+            }
     }
 }
--- src/BinaryTree/TreeTest.java
+++ src/BinaryTree/TreeTest.java
@@ -7,6 +7,34 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TreeTest {
         @Test
+        void canReturnNullRoot() {
+             //given
+             final Tree tree5 = new Tree(null);
+
+             //when
+                tree5.getRoot();
+
+             //then
+                assertEquals(tree5.getRoot(), null);
+
+        }
+
+        @Test
+        void canReturnRoot() {
+                //given
+                final Tree tree5 = new Tree(null);
+                final Tree.Node node5 = new Tree.Node(5);
+                final var root = tree5.getRoot();
+                //when
+                tree5.insert(root, 5);
+                tree5.getRoot();
+
+                //then
+                assertEquals(tree5.getRoot(), node5);
+
+        }
+
+        @Test
         void doesNodeAndValueExist() {
                 //given
                 final Tree.Node node1 = new Tree.Node(50);
@@ -36,28 +64,50 @@ class TreeTest {
                 assertNotEquals(node2.left.value, 40);
         }
 
-        @Test
-        void canNodesBeTraversed() {
+//        @Test
+//        void canNodesBeTraversed() {
+//
+//                //given
+//                final Tree.Node node3 = new Tree.Node(50);
+//                final Tree tree3 = new Tree(node3);
+//
+//                //when
+//                tree3.insert(node3,10);
+//                tree3.insert(node3,20);
+//                tree3.insert(node3,30);
+//                tree3.insert(node3,40);
+//                tree3.insert(node3,60);
+//                tree3.insert(node3,70);
+//                tree3.insert(node3,80);
+//                tree3.insert(node3,90);
+//                tree3.insert(node3,100);
+//
+//                //then
+//                tree3.traverseInOrder(node3);
+//        }
 
-                //given
-                final Tree.Node node3 = new Tree.Node(50);
-                final Tree tree3 = new Tree(node3);
+
+//        @Test
+//        void canDeleteNode() {
+//                //given
+//                final Tree.Node node4 = new Tree.Node(50);
+//                final Tree tree4 = new Tree(node4);
+//
+//
+//                //when
+//                tree4.insert(node4,10);
+//                tree4.insert(node4,20);
+//                tree4.insert(node4,30);
+//                tree4.insert(node4,40);
+//                tree4.insert(node4,60);
+//                tree4.insert(node4,70);
+//                tree4.insert(node4,80);
+//                tree4.insert(node4,90);
+//                tree4.insert(node4,100);
+//                tree4.deleteNode(node4, 100);
+//
+//                tree4.traverseInOrder(node4);
+//        }
 
 
-                //when
-                tree3.insert(node3,10);
-                tree3.insert(node3,20);
-                tree3.insert(node3,30);
-                tree3.insert(node3,40);
-                tree3.insert(node3,60);
-                tree3.insert(node3,70);
-                tree3.insert(node3,80);
-                tree3.insert(node3,90);
-                tree3.insert(node3,100);
-
-
-
-                //then
-                tree3.traverseInOrder(node3);
-        }
 }
